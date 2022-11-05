9249caa0efe05ee1a183a453585e0a4625fb39f2
Fadia04 <fl.medja@gmail.com>
2022-11-05 06:52:00 +0100

    first commit

--- purbeurre/purbeurre/settings.py
+++ purbeurre/purbeurre/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "static/"
-# STATICFILES_DIRS = [BASE_DIR.joinpath('static'),]
+STATICFILES_DIRS = [BASE_DIR.joinpath('static'),]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
--- purbeurre/purbeurre/tests/functional_tests/test_products.py
+++ purbeurre/purbeurre/tests/functional_tests/test_products.py
@@ -3,11 +3,15 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.urls import reverse
 from selenium.webdriver.common.by import By
 import time
+from selenium.webdriver.chrome.service import Service
 
 
 class TestSearchProduct(StaticLiveServerTestCase):
-    def test_search(self):
-        self.browser = webdriver.Chrome("purbeurre/tests/functional_tests/chromedriver")
+    def test_search(self):        
+        # Add lines 12 and 13 and suppress line 14 to resolve this warning:DeprecationWarning: executable_path has been deprecated, please pass in a Service object
+        #chrome_executable = Service(executable_path='chromedriver.exe', log_path='NUL')
+        self.browser = webdriver.Chrome()        
+        #self.browser = webdriver.Chrome("purbeurre/tests/functional_tests/chromedriver")
         self.browser.get(self.live_server_url + reverse("home"))
         search_product = self.browser.find_element(By.NAME, 'query')
         search_product.send_keys("nutella")
